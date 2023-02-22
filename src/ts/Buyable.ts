ac670457273aa53eb06a241db589a25e07215caa
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-02-22 18:33:46 +0300

    Add files via upload

--- src/ts/Buyable.ts
+++ src/ts/Buyable.ts
@@ -3,5 +3,7 @@ export default interface Buyable{
     readonly id: number,
     readonly name: string,
     readonly price: number,
+    readonly count: boolean,
+    amount: number,
 
 }
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,5 +1,8 @@
 import Cart from './Cart';
 import Movie from './Movie';
+import Book from './Books';
+import Phone from './Phone';
+import Music from './Music';
 
 
 const movie = new Movie(
@@ -15,7 +18,12 @@ true,
     1000,
     'avengers.png',
 );
+const music = new Music(1008, "Meteora", "Linkin Park", 900);
+const book = new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225);
 const cart = new Cart();
 
 cart.add(movie);
-cart.getAll();
+cart.add(music);
+cart.add(book);
+
+console.log(cart);
